5ffa8557638a4600bddf323aefc80d20f855c7b1
Sasha867 <sashacordoba@mail.ru>
2023-02-25 22:45:39 +0300

    feat: add function toReadable

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-module.exports = 
-function toReadable(number) {
-  const arrStr = number.toString().split("");
-  const twenty = "twenty ";
-  const hundred = "hundred ";
-  let value;
-  console.log(arrStr);
+module.exports = function toReadable(number) {
+    const arrStr = number.toString().split("");
+    const twenty = "twenty ";
+    const hundred = "hundred ";
+    let value;
+    console.log(arrStr);
 
-  if (arrStr.length === 1) {
-    value = primeNumbers.filter((el, index) => index + 1 === number);
-    value = value.join();
-  }
-  if (arrStr.length === 2 && arrStr[0] === "1") {
-    value = primeNumbers2.filter((el, index) => index === +arrStr[1]);
-    value = value.join();
-  }
-  if (arrStr.length === 2 && arrStr[0] === "2") {
-    value =
-      twenty + primeNumbers.filter((el, index) => index + 1 === +arrStr[1]);
-  }
+    if (arrStr.length === 1) {
+        value = primeNumbers.filter((el, index) => index + 1 === number);
+        value = value.join();
+    }
+    if (arrStr.length === 2 && arrStr[0] === "1") {
+        value = primeNumbers2.filter((el, index) => index === +arrStr[1]);
+        value = value.join();
+    }
+    if (arrStr.length === 2 && arrStr[0] === "2") {
+        value =
+            twenty +
+            primeNumbers.filter((el, index) => index + 1 === +arrStr[1]);
+    }
 
-  console.log(value);
+    console.log(value);
 
-  return value;
-}
+    return value;
+};
