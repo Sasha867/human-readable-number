75d365627eb39cc8a69d48ae6b097a7c7066ff7e
Sasha867 <sashacordoba@mail.ru>
2023-02-26 15:24:52 +0300

    feat: add function readable

--- src/index.js
+++ src/index.js
@@ -1,25 +1,69 @@
 module.exports = function toReadable(number) {
+    const primeNumbers = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+
+    const primeNumbers2 = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    const primeNumbers3 = [
+        "",
+        "",
+        "twenty ",
+        "thirty ",
+        "forty ",
+        "fifty ",
+        "sixty ",
+        "seventy ",
+        "eighty ",
+        "ninety ",
+    ];
     const arrStr = number.toString().split("");
-    const twenty = "twenty ";
-    const hundred = "hundred ";
+    const hundred = " hundred ";
     let value;
-    console.log(arrStr);
 
     if (arrStr.length === 1) {
-        value = primeNumbers.filter((el, index) => index + 1 === number);
+        value = primeNumbers.filter((el, index) => index === number);
         value = value.join();
     }
     if (arrStr.length === 2 && arrStr[0] === "1") {
         value = primeNumbers2.filter((el, index) => index === +arrStr[1]);
         value = value.join();
-    }
-    if (arrStr.length === 2 && arrStr[0] === "2") {
+    } else if (arrStr.length === 2 && arrStr[0] !== "1") {
         value =
-            twenty +
-            primeNumbers.filter((el, index) => index + 1 === +arrStr[1]);
+            primeNumbers3.filter((el, index) => index === +arrStr[0]) +
+            primeNumbers.filter((el, index) => index === +arrStr[1]);
+    }
+    if (arrStr.length === 3) {
+        value =
+            primeNumbers[arrStr[0]] +
+            hundred +
+            primeNumbers3[arrStr[1]] +
+            primeNumbers[arrStr[2]];
     }
 
-    console.log(value);
-
+    if (value.length !== primeNumbers[0].length) {
+        console.log("hi");
+        value = value.replace(" zero", "");
+    }
     return value;
 };
